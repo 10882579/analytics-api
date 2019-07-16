73ff04d0361bb9f5a70ca0582c91dc0c8e4c77a7
Botir, Bahodirov <benjamin10882579@yahoo.com>
2019-07-16 06:07:02 -0700

    server.js updated

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+const DB_LINK = process.env.MONGODB_URI || 'mongodb://localhost/analyticsdb';
+const ORIGIN = process.env.ORIGIN || 'http://localhost:3000';
+
+mongoose.connect(DB_LINK, {useNewUrlParser: true});
 mongoose.connection.on("open", () => {
   console.log('DB connection made');
 })
@@ -11,14 +14,14 @@ mongoose.connection.on("open", () => {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN);
+  res.setHeader('Access-Control-Allow-Origin', ORIGIN);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, X-Auth-Token');
 
   next();
 });
 
-require("./app/urls")(app);
+require("./src/urls")(app);
 
 app.listen(8000, () => {
   console.log('Server is running...');
